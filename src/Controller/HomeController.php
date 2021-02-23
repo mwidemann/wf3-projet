98eddf24e44473806f32d17eeab6882cc29efca4
Antoine-trisse <antoine.trisse@gmail.com>
2021-02-23 15:12:28 +0100

    integration contact form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,35 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $contactForm = $this->createForm(ContactType::class);
+        $contactForm->handleRequest($request);
+        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+            $contact = $contactForm->getData();
+            $mail = (new \Swift_Message('Lorem ipsum objet'))
+                ->setFrom($contact['email'])
+                ->setTo('antoine.trisse@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'contact/emailContact.html.twig', [
+                            'nom' => $contact['nom'],
+                            'prenom' => $contact['prenom'],
+                            'email' => $contact['email'],
+                            'objet' => $contact['objet'],
+                            'message' => $contact['message'],
+                        ]
+                    ),
+                    'text/html'
+                )
+            ;
+            $mailer->send($mail);
+            $this->addFlash(
+                'success',
+                'Votre message a bien été envoyé'
+            );
+            return $this->redirectToRoute('home');
+        }
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'contactForm' => $contactForm->createView(),
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,13 +5,44 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+            ->add('nom', TextType::class, [
+                'required' => true
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true
+            ])
+            // ->add('objet', ChoiceType::class, [
+            //     'required' => true,
+            //     'choices' => [
+            //         '- choix - ' => '',
+            //         'demande de devis' => 'devis',
+            //         'visiter un logement' => 'visite',
+            //         'postuler' => 'emploi',
+            //         'signaler un problème' => 'probleme'
+            //     ]
+            // ])
+            ->add('message', TextareaType::class, [
+                'attr' => [
+                    'minlength' => 50,
+                    'maxlength' => 1000
+                ],
+                'help' => '1000 caractères maximum'
+            ])
+            ->add('envoyer', SubmitType::class)
         ;
     }
 
