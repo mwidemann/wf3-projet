1adeb1f8092890b2a2a9531ee893d2d938beb919
Antoine-trisse <antoine.trisse@gmail.com>
2021-02-23 15:14:38 +0100

    integration contact form v2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request, \Swift_Mailer $mailer): Response
     {
         $contactForm = $this->createForm(ContactType::class);
         $contactForm->handleRequest($request);
