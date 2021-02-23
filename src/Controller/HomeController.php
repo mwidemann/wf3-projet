e2ccb97edaccd046e82df20a1dc37a7d9899a422
Antoine-trisse <antoine.trisse@gmail.com>
2021-02-23 15:20:09 +0100

    integration contact form v2 - ANNULATION

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,48 +2,19 @@
 
 namespace App\Controller;
 
-use App\Form\ContactType;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(Request $request, \Swift_Mailer $mailer): Response
+    public function index(): Response
     {
-        $contactForm = $this->createForm(ContactType::class);
-        $contactForm->handleRequest($request);
-        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
-            $contact = $contactForm->getData();
-            $mail = (new \Swift_Message('Lorem ipsum objet'))
-                ->setFrom($contact['email'])
-                ->setTo('antoine.trisse@gmail.com')
-                ->setBody(
-                    $this->renderView(
-                        'contact/emailContact.html.twig', [
-                            'nom' => $contact['nom'],
-                            'prenom' => $contact['prenom'],
-                            'email' => $contact['email'],
-                            'objet' => $contact['objet'],
-                            'message' => $contact['message'],
-                        ]
-                    ),
-                    'text/html'
-                )
-            ;
-            $mailer->send($mail);
-            $this->addFlash(
-                'success',
-                'Votre message a bien été envoyé'
-            );
-            return $this->redirectToRoute('home');
-        }
         return $this->render('home/index.html.twig', [
-            'contactForm' => $contactForm->createView(),
+            'controller_name' => 'HomeController',
         ]);
     }
 }
