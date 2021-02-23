4e3e41fe93851e0f89cf2117c1dfa88a779df922
Antoine-trisse <antoine.trisse@gmail.com>
2021-02-23 11:09:31 +0100

    Copie du cours

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,14 +18,10 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'prenom.nom@domaine.com'
+                ]
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
@@ -33,17 +29,39 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Merci de renseigner un mot de passe',
                     ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                    // new Length([
+                    //     'min' => 6,
+                    //     'minMessage' => 'Your password should be at least {{ limit }} characters',
+                    //     // max length allowed by Symfony for security reasons
+                    //     'max' => 4096,
+                    // ]),
+                    new PasswordStrength([
+                        'minLength' => 8,
+                        'tooShortMessage' => 'Le mot de passe doit contenir au moins 8 caractères',
+                        'minStrength' => 4,
+                        'message' => 'Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial'
+                    ])
+                ],
+                'attr' => [
+                    'placeholder' => '••••••••'
+                ],
+                'label' => 'Mot de passe'
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter nos conditions générales d\utilisation',
                     ]),
                 ],
+                'label' => 'J\'accepte les conditions générales d\'utilisation'
             ])
+            ->add('inscription', SubmitType::class) // Je l'ai repris du cours, mais je ne sais pas à quoi correspond cette ligne
         ;
+
+        // Il faudra sans doute ajouter toutes les autres données nécessaires pour l'entity user sous la même structure ?
     }
 
     public function configureOptions(OptionsResolver $resolver)
