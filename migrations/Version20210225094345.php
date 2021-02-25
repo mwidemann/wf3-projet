a891c87ae8be9c588071aef6104380b58b1e9a52
MorganeBequetRennes <mbequetrennes@gmail.com>
2021-02-25 12:12:10 +0100

    rien

--- migrations/Version20210225094345.php
+++ migrations/Version20210225094345.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210224084745 extends AbstractMigration
+final class Version20210225094345 extends AbstractMigration
 {
     public function getDescription() : string
     {
